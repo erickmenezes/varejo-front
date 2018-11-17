4a4b3546373ba1776cda0e0170adb2ff4163683f
Erick Menezes <erickbmenezes@gmail.com>
2018-11-17 17:44:37 -0200

    corrige o css da paginação do footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -8,35 +8,38 @@ class Footer extends Component {
           <div className="col-md-3 mb-3">
             <select className="form-control" name="show">
               <option value="4">4 produtos por página</option>
-              <option value="8">8 produtos por página</option>
+              <option value="8" selected>8 produtos por página</option>
               <option value="16">16 produtos por página</option>
               <option value="24">24 produtos por página</option>
             </select>
           </div>
-          <div className="col-md-3 my-auto ml-auto">
+
+          <div className="col-md-4 my-auto ml-auto">
             <div className="pagination text-center">
-              <a href="#">
-                <i class="far fa-window-minimize fa-rotate-270"></i>
-                <i class="fas fa-chevron-left"></i>
+              <a href="#" className="text-muted my-auto">
+                <i className="far fa-window-minimize fa-rotate-270"></i>
+                <i className="fas fa-chevron-left"></i>
               </a>
-              <a href="#" className="mx-3">
-                <i class="fas fa-chevron-left"></i>
+              <a href="#" className="text-muted mx-3 my-auto">
+                <i className="fas fa-chevron-left"></i>
               </a>
-              <div>
-                Páginas
-              </div> 
-              <a href="#" className="mx-3">
-                <i class="fas fa-chevron-right"></i>
+              <input type="text" className="form-control d-inline go-to" defaultValue="1"></input>
+              <a href="#" className="text-muted my-auto mx-3">2</a>
+              <a href="#" className="text-muted my-auto mx-3">3</a>
+              <a href="#" className="text-muted my-auto mx-3">4</a>
+              <a href="#" className="text-muted my-auto mx-3">5</a>
+              <a href="#" className="text-muted my-auto mx-3 my-auto">
+                <i className="fas fa-chevron-right"></i>
               </a>
-              <a href="#">
-                <i class="fas fa-chevron-right"></i>
-                <i class="far fa-window-minimize fa-rotate-90"></i>
+              <a href="#" className="text-muted my-auto">
+                <i className="fas fa-chevron-right"></i>
+                <i className="far fa-window-minimize fa-rotate-90"></i>
               </a>
             </div>
           </div>              
         </div>
       </footer>
-    );
+    ); 
   }
 }
  
