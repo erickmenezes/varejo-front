b2dfd9ce6ac62a7809c1ad71811a8f2394536c21
Erick Menezes <erickbmenezes@gmail.com>
2018-11-17 23:43:41 -0200

    recuperando as informações a serem apresentadas através da api

--- src/app/components/Content.js
+++ src/app/components/Content.js
@@ -3,25 +3,63 @@ import React, { Component } from 'react';
 import ProductCard from './ProductCard';
 
 class Content extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      search: null,
+      count: 0,
+      products: [],
+    };
+  }
+
+  componentWillMount() {
+    fetch('http://localhost:8080/products')
+      .then(response =>  response.json())
+      .then(json => {
+        this.setState({
+          search: null,
+          count: json.count,
+          products: json.results 
+        });
+      })
+      .catch((err) => {
+        console.log('An error occured');
+      });
+  }
+
+  renderSearchedField() {
+    if (this.state.search) {
+      return (
+        <div className="row">
+          <div className="col-md-12 title">
+            <h2 className="font-weight-light">{this.state.search}</h2>
+          </div>
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="content mb-3">
-        <div className="row">
-          <div className="col-md-12 title">
-            <h2 className="font-weight-light">TERMO BUSCADO</h2>
-          </div>
-        </div>
+        {this.renderSearchedField()}
         
         <div className="row">
           <div className="col-md-3">
             <div className="border-gold font-weight-light text-center text-uppercase my-4">
-              ### Produtos Encontrados
+              {this.state.count} Produtos Encontrados
             </div>
           </div>
         </div>
 
-      <ProductCard />
-
+      {this.state.products.map((product, index) => (
+        <div className="row" key={index}>
+          <div className="col-md-12">
+            <ProductCard key={index} product={product}/>
+          </div>
+        </div>
+      ))}
     </div>
     );
   }
--- src/app/components/ProductCard.js
+++ src/app/components/ProductCard.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
 
 class ProductCard extends Component {
+  formatPrice(price) {
+    return 'R$' + price.toFixed(2).replace(/\./,',').replace(/\d(?=(\d{3})+,)/g, '$&.');
+  }
+
   render() {
+    const product = this.props.product;
+
     return (
-      <div className="row">
-        <div className="col-md-12">
-          <div className="card">
-            <div className="card-body">
-              <div className="row">
-                <div className="col-md-5 my-auto">
-                  <img src="#" alt="IMAGEM 1"></img>
-                  <img src="#" alt="IMAGEM 2"></img>
-                  <img src="#" alt="IMAGEM 3"></img>
-                  <img src="#" alt="IMAGEM 4"></img>
-                </div>
-                <div className="col-md-4 my-auto">
-                  <h6 className="m-0">NOME PRODUTO</h6>
-                  <small className="text-muted">TIPO &#8226; MODELO</small>
-                </div>
-                <div className="col-md-3 my-auto">
-                  <span className="text-muted text-line-through">PRECO ANTIGO</span> 
-                  <span className="text-muted"> por </span>
-                  PREÇO NOVO
-                </div>
-              </div>
+      <div className="card">
+        <div className="card-body">
+          <div className="row">
+            <div className="col-md-5 my-auto">
+            {product.images.map((image, index) => (
+              <img className="img-product img-fluid" key={index} src={image.path} alt='Imagem do produto'></img>
+            ))}
+            </div>
+            <div className="col-md-4 my-auto">
+              <h6 className="m-0">{product.name}</h6>
+              <small className="text-muted">{product.type} &#8226; {product.size}</small>
+            </div>
+            <div className="col-md-3 my-auto">
+              <span className="text-muted text-line-through">{this.formatPrice(product.oldPrice)}</span> 
+              <span className="text-muted"> por </span>
+              {this.formatPrice(product.price)}
             </div>
           </div>
         </div>
